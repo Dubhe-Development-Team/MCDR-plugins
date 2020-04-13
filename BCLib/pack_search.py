4a3b9053cd863dbcb641ae846ac1a9c054e1e1d7
Felente <huudee@163.com>
2020-04-13 21:07:41 +0800

    update BCLib/pack_search.py. fix and refactor

--- BCLib/pack_search.py
+++ BCLib/pack_search.py
@@ -7,10 +7,5 @@ class Pack():
         metafile = rq.get(meta_link,timeout=60)
         self.isroot = isroot
         self.meta = metafile.json()
-
         self.downlist = []
-        self.needpack = []
-
-        if len(self.meta['lib'])>=0:
-            for lib in self.meta['lib']:
-                needpack.append(Pack(meta['lib']['meta'],server,False))
+        self.needpack = [needpack.append(lib['meta'],server,False) for lib in self.meta['lib']]
