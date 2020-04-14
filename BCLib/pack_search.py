469145c61b0cb939196f32db4def6e60a759a80a
WRC <55006226+HelloWRC@users.noreply.github.com>
2020-04-14 08:09:44 +0800

    Pack传引用

--- BCLib/pack_search.py
+++ BCLib/pack_search.py
@@ -3,9 +3,12 @@ import random as rand
 import json,os
 
 class Pack():
-    def __init__(self,meta_link,server,isroot=True):
+    def __init__(self,meta_link,server,isroot=True,downlist=[],needpack=[]):
         metafile = rq.get(meta_link,timeout=60)
         self.isroot = isroot
         self.meta = metafile.json()
+        self.downlist = downlist # 传递引用，使所有嵌套共用一个列表，以便管理
+        self.needpack = needpack
+
         self.downlist = []
-        self.needpack = [needpack.append(lib['meta'],server,False) for lib in self.meta['lib']]
+        self.needpack = [Pack(lib['meta'],server,False) for lib in self.meta['lib']]
