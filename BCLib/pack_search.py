a03bd7aa7c1d5365dd92e5b0199f67d79bf69aa8
WRC <hello_wrc@outlook.com>
2020-04-14 13:01:21 +0800

    轻微调整

--- BCLib/pack_search.py
+++ BCLib/pack_search.py
@@ -7,22 +7,30 @@ class Pack():
         "datapack":{},
         "pyplugin":{}
     },needpack=[]):
-        metafile = rq.get(meta_link,timeout=60)
+        try:
+            metafile = rq.get(meta_link,timeout=60)
+        except:
+            raise
         self.isroot = isroot
         self.meta = metafile.json()
         self.downlist = downlist # 传递引用，使所有嵌套共用一个列表，以便管理
         self.needpack = needpack
+        self.packname = self.meta['packname']
+        self.packlink = meta_link
+        
+        try:
+            self.needpack = [Pack(lib['meta'],server,False) for lib in self.meta['lib']]
 
-        self.needpack = [Pack(lib['meta'],server,False) for lib in self.meta['lib']]
+            for lib in self.meta['downloads']['datapack']:
+                self.downlist['datapack'][lib] = self.meta['downloads']['datapack'][lib]
+                server.logger.info('下载数据包文件源添加: {}'.format(self.downlist['datapack'][lib]))
+            for lib in self.meta['downloads']['pyplugin']:
+                self.downlist['pyplugin'][lib] = self.meta['downloads']['pyplugin'][lib]
+                server.logger.info('下载插件文件源添加: {}'.format(self.downlist['pyplugin'][lib]))
 
-        for lib in self.meta['downloads']['datapack']:
-            self.downlist['datapack'][lib] = self.meta['downloads']['datapack'][lib]
-            server.logger.info('下载数据包文件源添加: {}'.format(self.downlist['datapack'][lib]))
-        for lib in self.meta['downloads']['pyplugin']:
-            self.downlist['pyplugin'][lib] = self.meta['downloads']['pyplugin'][lib]
-            server.logger.info('下载插件文件源添加: {}'.format(self.downlist['pyplugin'][lib]))
-
-        server.logger.info('下载文件信息：{}'.format(str(downlist)))
+            server.logger.info('下载文件信息：{}'.format(str(downlist)))
+        except:
+            raise
 
     def get_downlist(self):
         return self.downlist
--- BridgeCaller.py
+++ BridgeCaller.py
@@ -15,7 +15,6 @@ try:
 except:
     try:
         from plugins.BCLib import *
-        
         print('MCDR env import')
     except:
         raise
@@ -39,13 +38,29 @@ def on_info(server,info):
     command = info.content.split(' ')
     if command[0] == '!!bc':
         if command[1] == 'install':
-            if HAVE_HELPER_PERMISSION(info,server):
-                # try:
-                downpack = pack_search.Pack(command[2],server)
-                # except:
-                #     if info.is_player:
-                #         server.tell(info.player,'§c参数错误！请使用!!bc查看帮助')
-                #     else:
-                #         server.logger.info('参数错误！请使用!!bc查看帮助')
-                #     return #back
+            if HAVE_HELPER_PERMISSION(info,server): 
+                try:
+                    downpack = pack_search.Pack(command[2],server)
+                except:
+                    if info.is_player:
+                        server.tell(info.player,'§c参数错误！请使用!!bc查看帮助')
+                    else:
+                        server.logger.info('参数错误！请使用!!bc查看帮助')
+                    return #back
+                downlist = downpack.downlist
+                packlist = downpack.needpack
+                if info.is_player:
+                    server.tell(info.player,"§l将要下载的数据包：(x{})".format(str(len(downlist['datapack']))))
+                    for dp in downlist['datapack']:
+                        server.tell(info.player,"- {}:§7§n{}".format(dp,downlist['datapack'][dp]))
+                    server.tell(info.player,"§l将要下载的插件：(x{})".format(str(len(downlist['pyplugin']))))
+                    for dp in downlist['pyplugin']:
+                        server.tell(info.player,"- {}:§7§n{}".format(dp,downlist['pyplugin'][dp]))
+                    server.tell(info.player,"§l将要下载的依赖包：(x{})".format(str(len(packlist))))
+                    for dp in packlist:
+                        server.tell(info.player,"- {}:§7§n{}".format(dp.packname,dp.packlink))
+                else:
+                    server.logger.info("将要下载的数据包：(x{})".format(str(len(downlist['datapack']))))
+                    for dp in downlist['datapack']:
+                        server.logger.info(info.player,"- {}:{}".format(dp,downlist['datapack'][dp]))
                 
