ebb1aae28d62976484a1d79899cb1aa8cfb631ca
Cjsah <2684117397@qq.com>
2020-05-10 19:17:35 +0800

    日期获取完成

--- BCLib/datapack_lib.py
+++ BCLib/datapack_lib.py
@@ -3,7 +3,8 @@
 
 '''
 import threading as thd
-
+import time
+from datetime import datetime
 
 global bgSRV
 bgSRV = None
@@ -19,7 +20,6 @@ def start_srv(server):
 def stop_srv(server):
     global STOP_SIGN
     STOP_SIGN = 1
-    
 
 
 def dpService(server):
@@ -28,3 +28,19 @@ def dpService(server):
     global STOP_SIGN
     while True:
         if STOP_SIGN:return
+
+
+def get_date(server):
+    server.execute("scoreboard objectives add bc_date dummy")
+
+    while True:
+        d = datetime.today()
+
+        server.execute("scoreboard players set #year bc.date " + str(d.year))
+        server.execute("scoreboard players set #month bc.date " + str(d.month))
+        server.execute("scoreboard players set #day bc.date " + str(d.day))
+        server.execute("scoreboard players set #hour bc.date " + str(d.hour))
+        server.execute("scoreboard players set #minute bc.date " + str(d.minute))
+        server.execute("scoreboard players set #second bc.date " + str(d.second))
+        server.execute("scoreboard players set #week bc.date " + str(d.isoweekday()))
+        time.sleep(1)
