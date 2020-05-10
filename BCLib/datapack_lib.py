db18ead4dcc484ecde52464584c8fa944307367e
WRC <hello_wrc@outlook.com>
2020-05-10 18:55:45 +0800

    datapack_lib线程部分完成

--- BCLib/datapack_lib.py
+++ BCLib/datapack_lib.py
@@ -7,23 +7,24 @@ import threading as thd
 
 global bgSRV
 bgSRV = None
+global STOP_SIGN
+STOP_SIGN = 0
 
 def start_srv(server):
-    # global bgSRV
-    # bgSRV = thd.Thread(target=lambda:dpService(server),name='Datapack_lib服务线程')
-    # bgSRV.start()
-    pass
+    global bgSRV
+    bgSRV = thd.Thread(target=lambda:dpService(server),name='Datapack_lib服务线程')
+    bgSRV.start()
+
 
 def stop_srv(server):
-    # global bgSRV
-    # bgSRV._stop()
-    # server.logger.info('Datapack_lib服务已终止')
-    pass
+    global STOP_SIGN
+    STOP_SIGN = 1
+    
 
 
 def dpService(server):
     '''数据包辅助服务'''
     server.logger.info('datapack_lib已启动')
+    global STOP_SIGN
     while True:
-        pass
-        #TODO: service code 
+        if STOP_SIGN:return
