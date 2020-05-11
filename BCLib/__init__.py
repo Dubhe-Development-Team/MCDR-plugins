7891922da34a0b3836ab1f9cd6160616f1c1dc1b
Cjsah <2684117397@qq.com>
2020-05-11 19:34:21 +0800

    忘记加功能库..

--- BCLib/__init__.py
+++ BCLib/__init__.py
@@ -8,7 +8,8 @@ bcapi：BridgeCaller API
 packobj：包对象文件
 datapack_lib：为数据包提供的API
 pack_actions：为包操作提供的函数库
+get_nbt： 读取nbt文件并调用
 """
 
 
-__all__ = ['packobj', 'datapack_lib', 'pack_actions', 'bcapi']
+__all__ = ['packobj', 'datapack_lib', 'pack_actions', 'bcapi', 'get_nbt']
