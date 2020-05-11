b8db220ff9bbd3015e310bb1d472ef4acaa537e1
WRC <hello_wrc@outlook.com>
2020-05-11 19:59:26 +0800

    lock.py：又只写了一个代码架构

--- BCLib/lock.py
+++ BCLib/lock.py
@@ -4,3 +4,13 @@ lock
 插件动作锁
 
 '''
+
+class Lock():
+    def __init__(self,main_action):
+        pass
+
+    def add_lock(self,action):
+        pass
+
+    def release_lock(self,action):
+        pass
