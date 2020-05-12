7afa05898e764c3087393c8a4d7a9602b5cec950
Cjsah <2684117397@qq.com>
2020-05-12 16:16:16 +0800

    随机数修了一下

--- BCLib/datapack_lib.py
+++ BCLib/datapack_lib.py
@@ -30,6 +30,7 @@ def dpService(server):
     """数据包辅助服务"""
     # service start code
     thd.Thread(target=lambda: get_time(server), name="BridgeCaller: 时间获取服务").start()
+    thd.Thread(target=lambda: rand(server), name="BridgeCaller: 随机数服务").start()
     server.logger.info('datapack_lib已启动')
     global STOP_SIGN
     while True:
@@ -42,17 +43,17 @@ def get_time(server):
     global STOP_SIGN
     d = datetime.today()
 
-    server.execute("scoreboard players set #year bc.time " + str(d.year))
-    server.execute("scoreboard players set #month bc.time " + str(d.month))
-    server.execute("scoreboard players set #day bc.time " + str(d.day))
-    server.execute("scoreboard players set #hour bc.time " + str(d.hour))
-    server.execute("scoreboard players set #minute bc.time " + str(d.minute))
-    server.execute("scoreboard players set #second bc.time " + str(d.second))
-    server.execute("scoreboard players set #week bc.time " + str(d.isoweekday()))
+    server.execute("scoreboard players set $year bc.time " + str(d.year))
+    server.execute("scoreboard players set $month bc.time " + str(d.month))
+    server.execute("scoreboard players set $day bc.time " + str(d.day))
+    server.execute("scoreboard players set $hour bc.time " + str(d.hour))
+    server.execute("scoreboard players set $minute bc.time " + str(d.minute))
+    server.execute("scoreboard players set $second bc.time " + str(d.second))
+    server.execute("scoreboard players set $week bc.time " + str(d.isoweekday()))
     if STOP_SIGN:
         return
 
 
 def rand(server):
     server.execute("scoreboard objectives add bc.rand dummy")
-    server.execute("scoreboard players set $random bc.rand " + random.randint(-100000, 100000))
+    server.execute("scoreboard players set $random bc.rand " + str(random.randint(-100000, 100000)))
