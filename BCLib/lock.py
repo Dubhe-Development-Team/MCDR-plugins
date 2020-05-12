ac830c66b71b0a550d22b6c8ad7911a626cae215
Cjsah <2684117397@qq.com>
2020-05-12 16:53:45 +0800

    改下代码规范

--- BCLib/lock.py
+++ BCLib/lock.py
@@ -1,16 +1,17 @@
-'''
+"""
 lock
 
 插件动作锁
 
-'''
+"""
+
 
 class Lock():
-    def __init__(self,main_action):
+    def __init__(self, main_action):
         pass
 
-    def add_lock(self,action):
+    def add_lock(self, action):
         pass
 
-    def release_lock(self,action):
+    def release_lock(self, action):
         pass
