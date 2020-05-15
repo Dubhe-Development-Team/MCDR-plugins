55088684e5aa28a469ff383fdc99252a8b15164a
Cjsah <2684117397@qq.com>
2020-05-15 18:37:14 +0800

    更改随机数和时间获取方式，防止刷屏

--- BCLib/datapack_lib.py
+++ BCLib/datapack_lib.py
@@ -3,12 +3,12 @@
 
 
 """
-
+import os
+import nbtlib
 import threading as thd
 import time
 import random
 from datetime import datetime
-from .get_nbt import start_srv as start_nbt_srv , stop_srv as stop_nbt_srv
 
 global bgSRV
 bgSRV = None
@@ -18,7 +18,6 @@ STOP_SIGN = 0   # debug
 
 def start_srv(server):
     global bgSRV
-    start_nbt_srv(server)
     bgSRV = thd.Thread(target=lambda: dpService(server), name='BridgeCaller: Datapack_lib服务线程')
     bgSRV.start()
 
@@ -26,38 +25,94 @@ def start_srv(server):
 def stop_srv(server):
     global STOP_SIGN
     STOP_SIGN = 1
-    stop_nbt_srv(server)
 
 
 def dpService(server):
     """数据包辅助服务"""
     # service start code
-    
+    thd.Thread(target=lambda: get_time(server), name="BridgeCaller: 时间获取服务").start()
     thd.Thread(target=lambda: rand(server), name="BridgeCaller: 随机数服务").start()
+    thd.Thread(target=lambda: get_seed(), name="BridgeCaller: 种子获取服务").start()
     server.logger.info('datapack_lib已启动')
     global STOP_SIGN
     while True:
-        get_time(server)
         if STOP_SIGN:
             return
 
 
 def get_time(server):
-    server.execute("scoreboard objectives add bc.time dummy")
+    server.execute("scoreboard objectives add bc dummy")
     global STOP_SIGN
-    d = datetime.today()
-
-    server.execute("scoreboard players set $year bc.time " + str(d.year))
-    server.execute("scoreboard players set $month bc.time " + str(d.month))
-    server.execute("scoreboard players set $day bc.time " + str(d.day))
-    server.execute("scoreboard players set $hour bc.time " + str(d.hour))
-    server.execute("scoreboard players set $minute bc.time " + str(d.minute))
-    server.execute("scoreboard players set $second bc.time " + str(d.second))
-    server.execute("scoreboard players set $week bc.time " + str(d.isoweekday()))
-    if STOP_SIGN:
-        return
+    while True:
+        if server.rcon_query("scoreboard players get #time bc") == "#time has 1 [bc]":
+            d = datetime.today()
+            server.execute("scoreboard players set #time bc 0")
+            server.execute("scoreboard objectives add bc.time dummy")
+            server.execute("scoreboard players set $year bc.time " + str(d.year))
+            server.execute("scoreboard players set $month bc.time " + str(d.month))
+            server.execute("scoreboard players set $day bc.time " + str(d.day))
+            server.execute("scoreboard players set $hour bc.time " + str(d.hour))
+            server.execute("scoreboard players set $minute bc.time " + str(d.minute))
+            server.execute("scoreboard players set $second bc.time " + str(d.second))
+            server.execute("scoreboard players set $week bc.time " + str(d.isoweekday()))
+        time.sleep(1)
+        if STOP_SIGN:
+            return
 
 
 def rand(server):
-    server.execute("scoreboard objectives add bc.rand dummy")
-    server.execute("scoreboard players set $random bc.rand " + str(random.randint(-100000, 100000)))
+    server.execute("scoreboard objectives add bc dummy")
+    global STOP_SIGN
+    while True:
+        if server.rcon_query("scoreboard players get #random bc") == "#random has 1 [bc]":
+            server.execute("scoreboard players set #random bc 0")
+            server.execute("scoreboard objectives add bc.rand dummy")
+            server.execute("scoreboard players set $random bc.rand " + str(random.randint(-100000, 100000)))
+        time.sleep(1)
+        if STOP_SIGN:
+            return
+
+
+# 获取世界种子
+def get_seed():
+    # 获取level.dat文件
+    path = os.path.join(get_path(), "world", "level.dat")
+    # 文件内容分成列表
+    levelnbt = str(nbtlib.load(path).root['Data']).split(', ')
+    # 读取失败赋值不变
+    seed = "读取失败"
+    for i in levelnbt:
+        # 获取种子
+        if i[0:10] == "RandomSeed":
+            seed = i[12:-1]
+            break
+    return "地图种子为： " + str(seed)
+
+
+# 获取玩家真实ID
+def get_id():
+    pass
+
+
+# 获取玩家背包
+def get_bag():
+    pass
+
+
+# 获取服务器目录
+def get_path():
+    path = ""
+    # 获取MCDRconfig文件
+    for i in open(os.path.join(os.getcwd(), "config.yml"), "r").readlines():
+        # 读取服务器目录
+        if i[0:17] == "working_directory":
+            for j in i[19:-1]:
+                # 目录中 "/" 转为 "\"
+                if j != "/":
+                    path += j
+                else:
+                    path += "\\"
+            # 获取完成
+            path = os.path.join(os.getcwd(), path)
+            break
+    return path
