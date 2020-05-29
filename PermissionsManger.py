d6e34b2c5a78839afe93919113352ec6ead01e58
Gu_ZT <administration@ztxy666.cn>
2020-05-29 21:39:37 +0800

    Update PermissionsManger.py

--- PermissionsManger.py
+++ PermissionsManger.py
@@ -18,3 +18,30 @@ PROGRAM = "pm","perm","permissions","perms","PermissionsManger"
 PLUGIN = PROGRAM
 NOT_PERMITTED = "§c权限不足"
 
+def on_load(server, old_module):
+    server.add_help_message('!!PermissionsManger', '权限管理')
+
+def on_info(server, info):
+    content = info.content.strip()[len(MCDR_PREFFIX):]
+    if not content.startswith(PLUGIN):
+        return
+    content = content[len(PLUGIN):].strip()
+    def generate_args(sub_cmd, msg_template):
+        pattern = '(?<={} )\S+'.format(sub_cmd)
+        for name in re.findall(pattern, content):
+            yield "{} {}".format(sub_cmd, name), msg_template.format(name)
+
+    def execute(cmd,  msg, error_msg, permission):
+        if server.get_permission_level(info) >= permission:
+            server.execute("{} {}".format(PROGRAM, cmd))
+            server.tell(info.player, msg)
+        else:
+            server.tell(info.player, error_msg)
+
+
+    def help():
+        server.tell(info.player, HELP_INFO)
+
+
+if __name__ == "__main__":
+    __test__()
